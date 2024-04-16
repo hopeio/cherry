c3aeac0375d678f8ecc97568fb49b6827ba35e22
贾一饼 <lby.i@qq.com>
2024-04-16 21:54:28 +0800

    fix: context error

--- context/log.go
+++ context/log.go
@@ -7,18 +7,18 @@ import (
 )
 
 func (c *RequestContext[REQ]) Error(args ...interface{}) {
-	args = append(args, zap.String(log.FieldTraceId, c.TraceID))
-	log.Default.Error(args...)
+	args = append(args, log.FieldTraceId, c.TraceID)
+	log.Error(args...)
 }
 
 func (c *RequestContext[REQ]) HandleError(err error) {
 	if err != nil {
-		log.Default.Error(err.Error(), zap.String(log.FieldTraceId, c.TraceID))
+		log.Errorw(err.Error(), zap.String(log.FieldTraceId, c.TraceID))
 	}
 }
 
 func (c *RequestContext[REQ]) ErrorLog(err, originErr error, funcName string) error {
 	// caller 用原始logger skip刚好
-	log.GetSkipLogger(1).Error(originErr.Error(), zap.String(log.FieldTraceId, c.TraceID), zap.Int(log.FieldType, errorcode.Code(err)), zap.String(log.FieldPosition, funcName))
+	log.GetSkipLogger(1).Errorw(originErr.Error(), zap.String(log.FieldTraceId, c.TraceID), zap.Int(log.FieldType, errorcode.Code(err)), zap.String(log.FieldPosition, funcName))
 	return err
 }
