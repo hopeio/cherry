c0235aaa976303bc58ad8538cfb7293ec1a717a4
贾一饼 <lby.i@qq.com>
2024-05-24 17:13:15 +0800

    fix: protogen linux

--- tools/protoc/protogen/protoc_linux.go
+++ tools/protoc/protogen/protoc_linux.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	execi "github.com/hopeio/cherry/utils/os/exec"
+	"strings"
 )
 
 // 直接执行protoc linux下会报 Could not make proto path relative: /xxx/*.proto: No such file or directory,找不到原因，无解
