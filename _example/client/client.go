e81d0ce700bb5a00fcc7431181720938e0ed4a3d
贾一饼 <lby.i@qq.com>
2025-10-29 19:32:10 +0800

    chore: fix import name

--- _example/client/client.go
+++ _example/client/client.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"context"
+
 	"github.com/hopeio/cherry/_example/protobuf/user"
 	"github.com/hopeio/gox/log"
-	grpci "github.com/hopeio/gox/net/http/grpc"
+	grpcx "github.com/hopeio/gox/net/http/grpc"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	conn, err := grpci.NewClient("localhost:8080", grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
+	conn, err := grpcx.NewClient("localhost:8080", grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
